ded724ad82d38b5b7afc68fb2e783b80319c97eb
Bayu Prathama <dev.bayuprathama@gmail.com>
2022-06-04 01:39:56 +0800

    initial app

---
+++ src/components/FeaturesCard.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import dashed from '../images/bg-dots.svg';
+import IconChrome from './media/IconChrome';
+
+export default function FeaturesCard({
+  title,
+  minimumVersion,
+  icon,
+  top,
+  className,
+}) {
+  return (
+    <div
+      className={`shadow-lg rounded-xl md:w-[280px] md:mt-[${top}] ${className}`}
+    >
+      {/* top */}
+      <div
+        style={{ backgroundImage: `url(${dashed})` }}
+        className="flex flex-col items-center justify-center bg-bottom bg-no-repeat"
+      >
+        <span className="mb-[25px] mt-[40px]">{icon}</span>
+        <div className="text-center mb-[40px]">
+          <p className="font-medium text-gray-800">{title}</p>
+          <p className="text-sm text-gray-500">{minimumVersion}</p>
+        </div>
+      </div>
+      {/* bottom */}
+      <div className="flex items-center justify-center py-7">
+        <button className="text-white bg-soft-blue py-[13px] px-8 font-medium rounded-lg text-sm">
+          Add & Install Extension
+        </button>
+      </div>
+    </div>
+  );
+}
